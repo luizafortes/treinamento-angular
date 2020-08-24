e7ba6004edbee695cba14ab2f6061a7761a7c2e2
Luiza Fortes <luizafortes@gmail.com>
2020-08-24 13:10:10 -0300

    aula 18 - Iniciando o projeto01 em Angular

---
+++ projeto01/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CadastroClienteComponent } from './cadastro-cliente/cadastro-cliente.component';
+import { ConsultaClienteComponent } from './consulta-cliente/consulta-cliente.component';
+
+//importando as bibliotecas necessárias do angular/router
+import { RouterModule, Routes } from '@angular/router';
+
+//mapeamento das rotas do projeto (navegações)
+const appRoutes : Routes = [
+  { path : 'cadastrar-clientes', component : CadastroClienteComponent },
+  { path : 'consultar-clientes', component : ConsultaClienteComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CadastroClienteComponent,
+    ConsultaClienteComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes) //registrando a configuração
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
